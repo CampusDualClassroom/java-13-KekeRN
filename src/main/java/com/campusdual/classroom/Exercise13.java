c9b431ded208072047dac580c065bb0e770cbcc3
KevinMontecastelo <Kevin.r.nunez@gmail.com>
2024-11-22 09:28:12 +0100

    Ej 13 Completado

--- src/main/java/com/campusdual/classroom/Exercise13.java
+++ src/main/java/com/campusdual/classroom/Exercise13.java
@@ -4,8 +4,12 @@ public class Exercise13 {
     public static void main(String[] args) {
         FuelTank ft = new FuelTank();
         ft.showDetails();
-        System.out.println("Actualización capacidad");
-        ft.actualFuel = -8;
+        System.out.println("Actualización capacidad a 20 litros");
+        ft.setActualFuel(20);
+        ft.showDetails();
+
+        System.out.println("Intento de actualización con valor negativo:");
+        ft.setActualFuel(-5);
         ft.showDetails();
     }
 }
--- src/main/java/com/campusdual/classroom/FuelTank.java
+++ src/main/java/com/campusdual/classroom/FuelTank.java
@@ -2,13 +2,23 @@ package com.campusdual.classroom;
 
 public class FuelTank {
 
-  public int actualFuel = 10;
+  private int actualFuel = 10;
 
   public void showDetails() {
+
     System.out.println("La capacidad actual es de " + this.actualFuel + " litros.");
   }
 
   public int getActualFuel() {
+
     return this.actualFuel;
+  }
+
+  public void setActualFuel(int fuel){
+    if(fuel<0) {
+      System.out.println("No se puede asignar un valor negativo a actualFuel.");
+    }else{
+      this.actualFuel = fuel;
+    }
   }
 }
